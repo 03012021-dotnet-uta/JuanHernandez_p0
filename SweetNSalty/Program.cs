c18ecfd73d18837449d9b559493bf55d2e849cf0
Juan Hernandez <juanhernandez0333@gmail.com>
2021-03-13 20:42:01 -0600

    fixed output not printing correct

--- SweetNSalty/Program.cs
+++ SweetNSalty/Program.cs
@@ -6,36 +6,42 @@ namespace SweetNSalty
     {
         static void Main(string[] args)
         {
+            var starCount = 1;
+            var endCount = 1000;
             var sweetCount = 0;
             var saltyCount = 0;
             var sweetNSaltyCount = 0;
 
             // For loop that counts to 1000
-            for (int i = 1; i <= 1000; i++)
+            for (; starCount <= endCount; starCount++)
             {
-                if (i % 3 == 0 && i % 5 == 0) // For numbers which are multiples of three and five, print “sweet’nSalty” to the console (instead of the number).  
+                if (starCount % 3 == 0 && starCount % 5 == 0) // For numbers which are multiples of three and five, print “sweet’nSalty” to the console (instead of the number).  
                 {
+                    if (starCount % 10 == 0)
+                    {
+                        Console.WriteLine("Sweet'N'Salty "); // After every 10 numbers start a new line of numbers
+                    }
                     Console.Write("Sweet'N'Salty ");
                     sweetNSaltyCount += 1; //counts sweet'n'salty
                 }
-                else if (i % 3 == 0) // When the number is multiple of three, print “sweet” instead of the number on the console.  
+                else if (starCount % 3 == 0) // When the number is multiple of three, print “sweet” instead of the number on the console.  
                 {
                     Console.Write("Sweet ");
                     sweetCount += 1; //counts sweet
                 }
-                else if (i % 5 == 0) // If the number is a multiple of five, print “salty” (instead of the number) to the console.  
+                else if (starCount % 5 == 0) // If the number is a multiple of five, print “salty” (instead of the number) to the console.  
                 {
                     Console.Write("Salty ");
                     saltyCount += 1; //counts salty
 
-                    if (i % 10 == 0)
+                    if (starCount % 10 == 0)
                     {
                         Console.WriteLine(); // After every 10 numbers start a new line of numbers
                     }
                 }
                 else
                 {
-                    Console.Write(i + " ");
+                    Console.Write(starCount + " ");
                 }
             }
 

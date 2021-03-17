c1effe56cc622cd944693aafc96ab31e94059211
Juan Hernandez <juanhernandez0333@gmail.com>
2021-03-17 18:22:28 -0500

    finished sweet'n'salty

--- SweetNSalty_JS/index.js
+++ SweetNSalty_JS/index.js
@@ -1,38 +1,43 @@
 var endCount = 1000;
+
 var sweetCount = 0;
 var saltyCount = 0;
 var sweetNSaltyCount = 0;
+
 var sweet = "Sweet";
 var salty = "Salty";
 var sweetNSalty = "Sweet'N'Salty";
 
-// For loop that counts to 1000
-for (var starCount = 1; starCount <= endCount; starCount++) {
-    if (starCount % 3 == 0 && starCount % 5 == 0) // For numbers which are multiples of three and five, print “sweet’nSalty” to the console (instead of the number).  
-    {
-        if (starCount % 10 == 0) {
-            console.log("Sweet'N'Salty "); // After every 10 numbers start a new line of numbers
-        }
-        console.log("Sweet'N'Salty ");
-        sweetNSaltyCount += 1; //counts sweet'n'salty
-    } else if (starCount % 3 == 0) // When the number is multiple of three, print “sweet” instead of the number on the console.  
-    {
-        console.log("Sweet ");
-        sweetCount += 1; //counts sweet
-    } else if (starCount % 5 == 0) // If the number is a multiple of five, print “salty” (instead of the number) to the console.  
-    {
-        console.log("Salty ");
-        saltyCount += 1; //counts salty
 
-        if (starCount % 10 == 0) {
-            console.log(); // After every 10 numbers start a new line of numbers
-        }
+var arry1 = []; //will hold num 1-1000
+var arry2 = []; //will hold our new array with the S
+var arry3 = []; //holds our output
+
+for (var i = 0; i <= endCount; i++) { //populating array
+    arry1[i] = i + 1;
+}
+
+for (var i = 0; i < 1000; i++) { //populates array2
+    if (arry1[i] % 3 == 0 && arry1[i] % 5 == 0) {
+        arry2[i] = sweetNSalty;
+        sweetNSaltyCount += 1; //counts sweet n salty
+    } else if (arry1[i] % 3 == 0) {
+        arry2[i] = sweet;
+        sweetCount += 1; //counts sweet
+    } else if (arry1[i] % 5 == 0) {
+        arry2[i] = salty;
+        saltyCount += 1; //counts salty
     } else {
-        console.log(starCount + " ");
+        arry2[i] = `${arry1[i]}`;
     }
 }
 
+while (arry2.length > 0) { //divides our array into 100 arrays
+    arry3 = arry2.splice(0, 10)
+    console.log(arry3)
+}
+
 //Shows the amount of times each word was counted
-console.log(`\n\n${sweet} Count: ` + sweetCount);
+console.log(`\n${sweet} Count: ` + sweetCount);
 console.log(`${salty} Count: ` + saltyCount);
 console.log(`${sweetNSalty} Count: ` + sweetNSaltyCount);
